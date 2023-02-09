5961dd9bf8630bb504e29b7a033b15581f92ec74
emanuelprass <emanuelpras4@gmail.com>
2023-02-09 18:01:26 +0700

    models - add mass-assignment protection

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Chirp extends Model
 {
     use HasFactory;
+
+    /*
+    Passing all of the data from a request to your model can be risky. 
+    Imagine you have a page where users can edit their profiles. 
+    If you were to pass the entire request to the model, then a user could edit any column they like, such as an is_admin column. 
+    This is called a mass assignment vulnerability.
+
+    Laravel protects you from accidentally doing this by blocking mass assignment by default. 
+    Mass assignment is very convenient though, as it prevents you from having to assign each attribute one-by-one. 
+    We can enable mass assignment for safe attributes by marking them as "fillable".
+    */
+    protected $fillable = [
+        'message',
+    ];
 }
